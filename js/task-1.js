d5eeaf13d456266e15a1ef23e6b87c75cb03725c
Mykyta_Rymar <mykitarymar@gmail.com>
2025-05-09 17:27:33 +0200

    task 1 fertig

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function makeTransaction (quantity, pricePerDroid) {
+     const totalprice = quantity * pricePerDroid;
+     const transaction = "You ordered " + quantity + " droids worth " + totalprice + " credits!";
+     return transaction;
+}
+
+console.log(makeTransaction(3, 1000));
